e7fdd836f74f042c752929507bb41f444a38bac7
Aubrey Loudenslager <aubrey.d.webster@gmail.com>
2023-05-08 15:00:24 -0700

    post container stuff

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,19 +2,34 @@ import React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import small from '../images/small.png';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { amber } from '@mui/material/colors';
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: amber[50],
+      },
+      secondary: {
+        main: '#5c6f59',
+      },
+    },
+  });
 
 const Navbar = () => {
     return (
-        <div classname='nav-bar'> Navbar
-            <Stack direction="row" spacing={2}>
-                <img className="small-logo" src={small}/>
-                <Button color="secondary">Secondary</Button>
-                <Button color="secondary">Secondary</Button>
-                <Button color="secondary">Secondary</Button>
-                <Button color="secondary">Secondary</Button>
-                <Button color="secondary">Secondary</Button>
-                <Button color="secondary">Secondary</Button>
-            </Stack>
+        <div id='nav-bar'>
+            <ThemeProvider theme={theme}>
+                <Stack direction="row" spacing={2}>
+                    <Button color="secondary" size='large'>Following</Button>
+                    <Button color="secondary" size='large'>Settings</Button>
+                    <Button color="secondary" size='large'>New Post</Button>
+                    <img className="small-logo" src={small}/>
+                    <Button color="secondary" size='large'>Search</Button>
+                    <Button color="secondary" size='large'>Profile</Button>
+                    <Button color="secondary" size='large'>Logout</Button>
+                </Stack>
+            </ThemeProvider>
         </div>
     )
 }
--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,17 +1,43 @@
-function PostCard(post){
-    const {image, description, category} = post;
+// function PostCard(post){
+//     const {image, description, category} = post;
    
-    return (
-    <div>
-        <div>
-            <img className="card-img" src={image} alt={category}/>
-            <h5 className="grid2">{description}</h5>
+//     return (
+//     <div>
+//         <div>
+//             <img className="card-img" src={image} alt={category}/>
+//             <h5 className="grid2">{description}</h5>
             
-            <h6 className="grid3">${category}</h6>       
-        </div>  
-     </div>
+//             <h6 className="grid3">${category}</h6>       
+//         </div>  
+//      </div>
 
-    )
+//     )
+// }
+
+// export default PostCard
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+
+export default function PostCard(post) {
+    const {image, description, category} = post;
+    return (
+    <Box sx={{ width: 500, height: 450, overflowY: 'scroll' }}>
+      <ImageList variant="masonry" cols={3} gap={8}>
+      {/* {post.map((post) => ( */}
+          <ImageListItem key={image}>
+            <img
+              src={`${image}?w=248&fit=crop&auto=format`}
+              srcSet={`${image}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={description}
+              loading="lazy"
+            />
+            <ImageListItemBar position="below" title={description}/>
+          </ImageListItem>
+      </ImageList>
+    </Box>
+    );
 }
-
-export default PostCard
