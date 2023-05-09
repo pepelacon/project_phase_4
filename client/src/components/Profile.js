3ea1e57b1e8b7e2a8921b0e8ee44ede4d36a6a92
Daniel <pepelacon@gmail.com>
2023-05-09 15:41:17 -0400

    resolve issue

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,7 +1,32 @@
-const Profile = () => {
+import { useAuth0 } from '@auth0/auth0-react'
+import { useEffect } from 'react'
+
+const ProFile = () => {
+
+    const { user, isAuthenticated, isLoading } = useAuth0();
+    
+    useEffect(() => {
+        if (user) {
+            fetch('/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: user.name,
+                email: user.email
+            })
+        });
+            
+        }
+    }, [user])
+
+
     return (
-        <div>This is the user profile page</div>
+        <>
+        </>
     )
+
 }
 
-export default Profile
+export default ProFile
