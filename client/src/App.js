16e50c0188b4ea6ed361fa99fd03c1b124b01569
Aubrey Loudenslager <aubrey.d.webster@gmail.com>
2023-05-09 12:30:12 -0700

    delete homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import PostContainer from './components/PostContainer'
-import HomePage from './components/HomePage';
 import Profile from './components/Profile';
 import Following from './components/Following';
 import Navbar from './components/Navbar';
