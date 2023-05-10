e238401a6e038b8ac0741bbb44e171eac870038a
Daniel <pepelacon@gmail.com>
2023-05-10 13:56:39 -0400

    la

--- server/app.py
+++ server/app.py
@@ -2,7 +2,8 @@ from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
+from dotenv import load_dotenv
+load_dotenv()
 
 from models import db, User, Friendship, Post
 
